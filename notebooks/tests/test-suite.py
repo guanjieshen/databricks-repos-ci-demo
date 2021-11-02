b599b977becabb64b478445f2fa22093105f08d3
guanjieshen <guanjieshen>
2021-11-02 00:23:54 +0000

    chore: add unit test example

--- notebooks/tests/test-suite.py
+++ notebooks/tests/test-suite.py
@@ -1,10 +1,14 @@
 # Databricks notebook source
 # DBTITLE 1,Install nutter and it's dependencies
-# MAGIC %pip install nutter
+# MAGIC %pip install -U nutter chispa
 
 # COMMAND ----------
 
-# DBTITLE 1,Define testing classes - release 0.0.1
+# MAGIC %md ### Test `prepare-data.py`
+
+# COMMAND ----------
+
+# DBTITLE 1,Test prepare-data
 from runtime.nutterfixture import NutterFixture, tag
 
 
@@ -14,7 +18,7 @@ class PrepareDataTestFixture(NutterFixture):
 
     def assertion_test_name(self):
         counter = spark.sql(
-            "SELECT COUNT(*) AS total FROM delta.`dbfs:/databricks-repos-ci-demo/data/boston`"
+            "SELECT COUNT(*) AS total FROM delta.`dbfs:/databricks-ci-demo/data/boston`"
         )
         first_row = counter.first()
         assert first_row[0] > 1
@@ -22,6 +26,33 @@ class PrepareDataTestFixture(NutterFixture):
 
 result = PrepareDataTestFixture().execute_tests()
 print(result.to_string())
+
+# COMMAND ----------
+
+# MAGIC %md ### Test `Common.py`
+
+# COMMAND ----------
+
+# MAGIC %run ../src/common
+
+# COMMAND ----------
+
+from chispa.dataframe_comparer import *
+
+class CommonTestFixture(NutterFixture):
+    
+  # we're using Chispa library here to compare the content of the processed dataframe with expected results
+  def assertion_upper_columns(self):
+    cols = ["col1", "col2", "col3"]
+    df = spark.createDataFrame([("abc", "cef", 1)], cols)
+    upper_df = upper_columns(df, cols)
+    expected_df = spark.createDataFrame([("ABC", "CEF", 1)], cols)
+    assert_df_equality(upper_df, expected_df)
+      
+result = CommonTestFixture().execute_tests()
+print(result.to_string())
+
+# COMMAND ----------
 
 is_job = (
     dbutils.notebook.entry_point.getDbutils()
